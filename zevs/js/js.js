c00a5dc42a7f3529d6cd677cf3fc6c0b56d4f40a
Oleg <zugan88@yandex.ru>
2019-04-02 10:57:31 +0300

    git add

--- zevs/js/js.js
+++ zevs/js/js.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
 	//E-mail Ajax Send
-	$("form").submit(function() { //Change
+	$(".block_ten__form").submit(function() { //Change
 		var th = $(this);
 		$.ajax({
 			type: "POST",
