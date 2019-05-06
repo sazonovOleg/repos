5d344b93ddd849da7ceb61ce1b5dd2894361abff
Oleg <zugan88@yandex.ru>
2019-05-06 15:02:13 +0300

    git add

--- work/js/js.js
+++ work/js/js.js
@@ -33,3 +33,23 @@ $(".all").click(function () {
     $("[class*=test]").show();
     $("[class*=isp]").show();
 });
+
+$(function(){
+  $('.work_row__columns').click(function(event) {
+    var i_path = $(this).attr('src');
+    $('body').append('<div id="overlay"></div><div id="magnify"><img src="'+i_path+'"><div id="close-popup"><i></i></div></div>');
+    $('#magnify').css({
+	    left: ($(document).width() - $('#magnify').outerWidth())/2,
+            top: ($(window).height() - $('#magnify').outerHeight())/2
+	  });
+    $('#overlay, #magnify').fadeIn('fast');
+  });
+  
+  $('body').on('click', '#close-popup, #overlay', function(event) {
+    event.preventDefault();
+ 
+    $('#overlay, #magnify').fadeOut('fast', function() {
+      $('#close-popup, #magnify, #overlay').remove();
+    });
+  });
+});
