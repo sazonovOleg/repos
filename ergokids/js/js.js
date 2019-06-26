69b78a34e8b015f044dfe358d24c481f85378d57
Oleg <zugan88@yandex.ru>
2019-06-26 15:49:54 +0300

    Update js.js

--- ergokids/js/js.js
+++ ergokids/js/js.js
@@ -11,21 +11,21 @@ const headerTitle = document.querySelector('.header_title'),
 //      
 //    });
 	$(".orange").click(function () {
-		$(".header_title").css({'color':'#ff7d3d'});
-        $(".main_iner").css({'border-color':'#ff7d3d'});
-        $(".main_iner__brd").css({'border-color':'#ff7d3d'});
-        $(".main_title").css({'color':'#ff7d3d'});
-        $(".wrap_icon").css({'background':'#ff7d3d'});
-        $(".footer_btn").css({'background':'#ff7d3d'});
-        $(".circle").css({'border-color':'#ff7d3d'});
-        $(".circle_block").css({'background':'#ff7d3d'});
-        $(".footer_iner__title").css({'color':'#ff7d3d'});
-        $(".chair.orange").css({'opacity':'1'});
-        $(".chair.green").css({'opacity':'0'});
-        $(".main_green").css({'opacity':'0'});
-        $(".main_orange").css({'opacity':'1'});
-        $(".line_green").css({'opacity':'0'});
-        $(".line_orange").css({'opacity':'1'});
+		$(".header_title").css({'color':'#ff7d3d','transition':'0.6s all ease'});
+        $(".main_iner").css({'border-color':'#ff7d3d','transition':'0.6s all ease'});
+        $(".main_iner__brd").css({'border-color':'#ff7d3d','transition':'0.6s all ease'});
+        $(".main_title").css({'color':'#ff7d3d','transition':'0.6s all ease'});
+        $(".wrap_icon").css({'background':'#ff7d3d','transition':'0.6s all ease'});
+        $(".footer_btn").css({'background':'#ff7d3d','transition':'0.6s all ease'});
+        $(".circle").css({'border-color':'#ff7d3d','transition':'0.6s all ease'});
+        $(".circle_block").css({'background':'#ff7d3d','transition':'0.6s all ease'});
+        $(".footer_iner__title").css({'color':'#ff7d3d','transition':'0.6s all ease'});
+        $(".chair.orange").css({'opacity':'1','transition':'0.6s all ease'});
+        $(".chair.green").css({'opacity':'0','transition':'0.6s all ease'});
+        $(".main_green").css({'opacity':'0','transition':'0.6s all ease'});
+        $(".main_orange").css({'opacity':'1','transition':'0.6s all ease'});
+        $(".line_green").css({'opacity':'0','transition':'0.6s all ease'});
+        $(".line_orange").css({'opacity':'1','transition':'0.6s all ease'});
 		});
 
 	$(".green").click(function () {
