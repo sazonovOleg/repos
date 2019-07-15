a189ad3756400ec797852d8d38163319bf55e62f
Oleg <zugan88@yandex.ru>
2019-07-15 18:30:04 +0300

    git add

--- stomatology/js/js.js
+++ stomatology/js/js.js
@@ -6,3 +6,5 @@ $('.navbar-toggler-icon').click(function () {
     $('.nav-mobile__menu').css('opacity', '1').css('transition', '0.7s').css('z-index', '100');
     $('.navbar-toggler-icon').css('opacity', '0.3').css('transition', '0.7s');
 });
+$("#phone").mask("+7 (999) 999 - 9999",{autoclear: false});
+$("#phoneOne").mask("+7 (999) 999 - 9999",{autoclear: false});
