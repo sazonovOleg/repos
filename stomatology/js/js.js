a8219eb0882843b6e1dd95544d71a969449cbca9
Oleg <zugan88@yandex.ru>
2019-07-16 09:30:30 +0300

    git add

--- stomatology/js/js.js
+++ stomatology/js/js.js
@@ -1,10 +1,14 @@
-$('.nav-mobile__close').click(function () {
-    $('.nav-mobile__menu').css('opacity', '0').css('transition', '0.7s').css('z-index', '-1');
-    $('.navbar-toggler-icon').css('opacity', '1').css('transition', '0.7s');
+//$('.nav-mobile__close').click(function () {
+//    $('.nav-mobile__menu').css('opacity', '0').css('transition', '0.7s').css('z-index', '-1');
+//    $('.navbar-toggler-icon').css('opacity', '1').css('transition', '0.7s');
+//});
+//$('.navbar-toggler-icon').click(function () {
+//    $('.nav-mobile__menu').css('opacity', '1').css('transition', '0.7s').css('z-index', '100');
+//    $('.navbar-toggler-icon').css('opacity', '0.3').css('transition', '0.7s');
+//});
+$("#phone").mask("+7 (999) 999 - 9999", {
+    autoclear: false
 });
-$('.navbar-toggler-icon').click(function () {
-    $('.nav-mobile__menu').css('opacity', '1').css('transition', '0.7s').css('z-index', '100');
-    $('.navbar-toggler-icon').css('opacity', '0.3').css('transition', '0.7s');
+$("#phoneOne").mask("+7 (999) 999 - 9999", {
+    autoclear: false
 });
-$("#phone").mask("+7 (999) 999 - 9999",{autoclear: false});
-$("#phoneOne").mask("+7 (999) 999 - 9999",{autoclear: false});
