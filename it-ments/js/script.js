0ab51ca70f427747e51fbebc49c8235cc106df51
sazole <sazole@rarus.ru>
2020-05-28 13:12:17 +0300

    Правки по макету под десктоп

--- it-ments/js/script.js
+++ it-ments/js/script.js
@@ -1,7 +1,33 @@
-$(document).ready(function() {
-    $('.nav-burger').click(function() {
+$(document).ready(function () {
+    $('.nav-burger').click(function () {
         $('.nav').toggleClass("active");
         $('html').removeClass("active");
     })
 });
+
 new WOW().init();
+
+$('.multiple-items').slick({
+    infinite      : true,
+    slidesToShow  : 3,
+    slidesToScroll: 3,
+    dots          : true,
+    prevArrow     : false,
+    nextArrow     : false,
+    autoplay      : true,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3
+            }
+        },
+        {
+            breakpoint: 767,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
